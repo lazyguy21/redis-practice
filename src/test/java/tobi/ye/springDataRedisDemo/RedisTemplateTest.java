1e71043ed9da2b03abd592b2026213ca068d3543
ye <yyf@xiaomaihd.com>
2015-10-15 15:23:40 +0800

    init

---
+++ src/test/java/tobi/ye/springDataRedisDemo/RedisTemplateTest.java
@@ -0,0 +1,23 @@
+package tobi.ye.springDataRedisDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by ye on 15-10-10.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring*.xml")
+public class RedisTemplateTest{
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Test
+    public void test(){
+        stringRedisTemplate.opsForValue().set("myname","fromxxcaonima");
+    }
+}
